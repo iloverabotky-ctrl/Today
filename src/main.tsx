0ee0f6446aae860e4bc58433a27d7921a03b520f
iloverabotky-ctrl <iloverabotky@gmail.com>
2026-08-29 21:32:26 +0300

    Load exact return attention picker styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 import './status-separators.css';
+import './waiting-picker.css';
 
 const STORAGE_KEY = 'today-cockpit-v2';
 
