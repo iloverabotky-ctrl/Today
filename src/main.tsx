3b565d1ad21d1e6ffbe0f9a398716a6461221706
iloverabotky-ctrl <iloverabotky@gmail.com>
2026-08-30 14:10:33 +0300

    Load notebook title two-line fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './styles.css';
 import './team-oneonone.css';
 import './v6.css';
+import './v6-fixes.css';
 import { initSafeManagementWorkspace } from './management-safe';
 
 const STORAGE_KEY = 'today-cockpit-v2';
