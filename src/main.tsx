e3e3ef5ba92571df4e5d07930a27e47102bc000e
iloverabotky-ctrl <iloverabotky@gmail.com>
2026-08-30 15:20:40 +0300

    Load calm notebook interaction fixes

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import './notebook-stage-fix.css';
 import './notebook-history.css';
 import './task-focus.css';
 import './notebook-calm.css';
+import './notebook-calm-fix.css';
 import { initSafeManagementWorkspace } from './management-safe';
 
 const STORAGE_KEY = 'today-cockpit-v2';
