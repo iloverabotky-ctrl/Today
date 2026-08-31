5de3d7723e10e3f2450aeff857b25bbf1eed393f
iloverabotky-ctrl <iloverabotky@gmail.com>
2026-08-31 20:48:18 +0300

    Use rebuilt notebook v5 styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,8 @@ import './styles.css';
 import './team-oneonone.css';
 import './v6.css';
 import './v6-fixes.css';
-import './notebook-classic.css';
-import './notebook-history.css';
 import './task-focus.css';
-import './notebook-calm.css';
-import './notebook-calm-fix.css';
-import './notebook-v5.css';
+import './notebook-v5-full.css';
 import { initSafeManagementWorkspace } from './management-safe';
 
 const STORAGE_KEY = 'today-cockpit-v2';
